2358a5b99c40caef17f1641b0135afe97fa0e64c
iwallow <iwallow0@gmail.com>
2022-07-31 18:56:43 +0900

    complete exercise 1.12-1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,10 +12,49 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0, 0])
+
+  const toggleAnecdotes = () =>{
+    const num = Math.floor(Math.random() * anecdotes.length)
+    setSelected(num)
+  }
+
+  // 1.14 输出投票数最高的名言以及票数
+  // 有问题！！！！
+  // 直接在App的输出中调用函数，是否不太合适？
+  const getMostVoteAnecdote = () => {
+    const index = points.indexOf(Math.max(...points))
+    return(
+      <div>
+        <p>{anecdotes[index]}</p>
+        <p>has {points[index]} votes</p>
+      </div>
+    )
+  }
+
+  // 1.13 增加了投票机制，用数组储存每个名言的投票数
+  const toggleVote = (id) => {
+    const newPoints = [...points]
+
+    // 有问题！！！！
+    // 这里直接改变了副本的值，是否会有问题？
+    newPoints[id] += 1
+
+    console.log(newPoints)
+    setPoints(newPoints)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={() => toggleVote(selected)}>vote</button>
+      <button onClick={toggleAnecdotes}>next anecdotes</button>
+
+
+      <h1>Anecdote with most votes</h1>
+      {getMostVoteAnecdote()}
     </div>
   )
 }
