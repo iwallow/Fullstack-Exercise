a3ad3b7ae5bb50e5cec6ed30f0db0827ba01690b
iwallow <iwallow0@gmail.com>
2022-07-31 15:34:56 +0900

    complete exercise 1.1-1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,56 @@
+const Header = ({ course }) => (
+  <h1>
+    {course}
+  </h1>
+)
+
+const Part = ({part, exercise}) => (
+  <p>
+    {part} {exercise}
+  </p>
+)
+
+const Content = ({courses}) => (
+  <div>
+    <Part part={courses[0].part} exercise={courses[0].exercise} />
+    <Part part={courses[1].part} exercise={courses[1].exercise} />
+    <Part part={courses[2].part} exercise={courses[2].exercise} />
+  </div>
+)
+
+const Total = ({courses}) => (
+  <p>Number of exercise {courses[0].exercise + courses[1].exercise + courses[2].exercise}</p>
+)
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+
+  const courses = [
+    {
+      part: 'Fundamentals of React',
+      exercise: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercise: 7
+    },
+    {
+      part: 'State of a component',
+      exercise: 14
+    }
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content courses={courses} />
+      <Total courses={courses} />
     </div>
   )
 }
